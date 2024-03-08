424900939df916f6441c80e6ef555c675cefb1b8
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-03-08 10:04:33 +0100

    Feed page fix

--- models/other.py
+++
@@ -1,6 +0,0 @@
-from models.db import db
-
-
-class AlembicVersion(db.Model):
-    __tablename__ = "alembic_version"
-    version_num = db.Column(db.String(32), primary_key=True)
--- static/js/profile_page.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".link-danger").forEach(link => {
-  link.addEventListener("click", e => {
-    if (!confirm("Are you sure you want to delete this post?")) {
-      e.preventDefault();
-    }
-  });
-});
